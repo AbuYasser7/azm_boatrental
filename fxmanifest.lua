bcfa4e9f2fc3aff2e4943ca5c5148d86506a871f
AbuYasser <buzayd22@gmail.com>
2025-10-22 18:41:56 +0300

    fix

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,28 +1,28 @@
 fx_version 'cerulean'
 game 'gta5'
-lua54 'yes'
 
 name 'azm_boatrental'
 author 'abuyasser'
-description 'Al Azm Boat Rental System for Al Azm County'
+description 'Al Azm Boat Rental'
 version '1.0.0'
+lua54 'yes'
 
 shared_scripts {
     '@ox_lib/init.lua',
-    'config.lua',
-    'locales/*.lua'
+    'shared/config.lua'
 }
 
 client_scripts {
-    'client.lua'
+    'client/client.lua'
 }
 
 server_scripts {
     '@oxmysql/lib/MySQL.lua',
-    'server/main.lua'
+    'server/server.lua'
 }
 
 dependencies {
+    'oxmysql',
     'ox_lib',
-    'oxmysql'
+    'ox_target'
 }
